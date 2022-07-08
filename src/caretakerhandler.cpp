0f64754d90240faa91bf4c229dd9b0412303d626
Logan Kenwright <lken274@UoA.auckland.ac.nz>
2022-07-08 12:20:00 +1200

    binary

--- src/caretakerhandler.cpp
+++ src/caretakerhandler.cpp
@@ -128,7 +128,7 @@ void LIBCTAPI cb_on_data_received(libct_context_t *context, libct_device_t *devi
         if (handler == 0) throw std::runtime_error(std::string("Couldn't find handler"));
         //libct_vitals_t* vitals = libct_get_last_dp(data,vitals);
         //handler->hd.recentData["vitals"].timestamp = vitals->timestamp;
-        handler->hd.recentData["device_status"].timestamp = data->device_status.timestamp;
+        //handler->hd.recentData["device_status"].timestamp = data->device_status.timestamp;
     } 
     catch(const std::exception& e) {
     }
